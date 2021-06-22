f1466d5c0bc181c570b25c579796778c6112531b
mate0091 <mate.kelemen009@gmail.com>
2021-06-22 17:21:46 +0300

    sha256 token generated

--- api.py
+++ api.py
@@ -3,13 +3,14 @@ import json
 import base64
 import cv2
 import pickle
+import hashlib
 
 
 def image(request):
     if request.method == "POST":
-        data = request.body
+        data = json.loads(request.body)
 
-        image1 = base64.decodebytes(data)
+        image1 = base64.b64decode(data['imgbase64'])
 
         with open('image.png', 'wb') as f:
             f.write(image1)
@@ -23,10 +24,13 @@ def image(request):
         with open("labels.dat", 'rb') as f:
             temp_labels = pickle.load(f)
             labels = {v: k for k, v in temp_labels.items()}
-        #conv to grayscale
+        # conv to grayscale
         image = cv2.imread('image.png')
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+
+        name = None
+        conf = None
 
         if len(faces) != 0:
             x, y, w, h = faces[0]
@@ -39,8 +43,8 @@ def image(request):
 
             print(labels[ind], conf)
 
-            if 40 <= conf <= 65:
-                #print(labels[ind])
+            if 40 <= conf <= 85:
+                # print(labels[ind])
                 font = cv2.FONT_HERSHEY_SIMPLEX
                 name = labels[ind]
                 print(name)
@@ -55,7 +59,10 @@ def image(request):
 
         cv2.imwrite('webcam_detected.png', image)
 
+        if name == data['person']:
+            return JsonResponse({'person': 'OK', 'token': hashlib.sha256(name.encode('utf-8')).hexdigest()}, safe=False)
+        else:
+            return JsonResponse({'person': 'undefined'})
 
-        return JsonResponse({'post method': 'successful'}, safe=False)
     else:
         return JsonResponse({'get method': 'invalid'}, safe=False)
